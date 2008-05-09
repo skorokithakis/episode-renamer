155d3666ecf3c2175b2e78ad45648399c4638d53
Stavros Korokithakis <stavros@korokithakis.net>
2008-05-09 19:49:07 +0300

    Fixed epguides.com bug, added Google searching.

--- episode-renamer.py
+++ episode-renamer.py
@@ -4,6 +4,7 @@
 # Released under the GNU GPL.
 # You can find the latest version at http://www.poromenos.org
 
+import urllib
 import urllib2
 import optparse
 import re
@@ -17,14 +18,30 @@ from version import *
 
 SITES = [
     {"url": "http://epguides.com/%s/",
-     "episode_re": "\d+. +(?P<season>\d+) *\- *(?P<episode>\d+) +\d+ +[A-Za-z]+ +\d+ +<a target.*?>(?P<name>.*?)</a>",
+     "episode_re": "\d+. +(?P<season>\d+) *\- *(?P<episode>\d+) +(?:\d+|) +\d+ +[A-Za-z]+ +\d+ +<a target.*?>(?P<name>.*?)</a>",
      "title_re": """<h1><a href="http://.*?">(.*?)</a></h1>""",
+     "domain": "epguides.com",
+     "urlparser": "epguides.com\/(.*?)\/",
      },
     {"url": "http://www.imdb.com/title/%s/epdate",
      "episode_re": """<td align="right" bgcolor="#eeeeee">(?P<season>\d+)\.(?P<episode>\d+).?</td> *<td><a .*?>(?P<name>.*?)</a></td>""",
      "title_re": "<title>\"?(.*?)\"? *?\(.*?\)</title>",
+     "domain": "imdb.com",
+     "urlparser": "imdb\.com\/title\/(.*?)\/",
     },
         ]
+
+def search_show(name, site):
+    """Search Google for the page best matching the given show name."""
+    google_url = "http://www.google.com/xhtml/search?q=site%%3A%s+%s" % (site["domain"], urllib.quote(name))
+
+    # Bastard Google...
+    request = urllib2.Request(google_url)
+    request.add_header("User-Agent", "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.4) Gecko/20070515 Firefox/2.0.0.4")
+    page = urllib2.urlopen(request).read()
+    result = re.findall("<span class=\"green\">(.*?)</span>", page)[0]
+    show_id = re.search(site["urlparser"], result).group(1)
+    return show_id
 
 def parse_page(page, site):
     """Parse a page, entering the episode names in a dictionary."""
@@ -43,8 +60,8 @@ def parse_page(page, site):
 
 def rename_files(episode_names, preview=False, use_ap=False):
     series_parser = [
-        re.compile("^.*?s *?(?P<series>\d+) *?e *?(?P<episode>\d+).*?\.(?P<extension>.*?)$", re.IGNORECASE),
-        re.compile("^.*?(?P<series>\d+)x(?P<episode>\d+).*?\.(?P<extension>.*?)$", re.IGNORECASE),
+        re.compile("^.*?s *(?P<series>\d+) *e *(?P<episode>\d+).*\.(?P<extension>.*?)$", re.IGNORECASE),
+        re.compile("^.*?(?P<series>\d+)x(?P<episode>\d+).*\.(?P<extension>.*?)$", re.IGNORECASE),
         ]
     title = episode_names["title"]
     for filename in os.listdir("."):
@@ -63,10 +80,9 @@ def rename_files(episode_names, preview=False, use_ap=False):
         extension = match_dict["extension"]
 
         try:
-            print `episode_names[(series, episode)].encode("ascii", "replace")`
             new_filename = u"%s - S%02dE%02d - %s.%s" % (title, series, episode, episode_names[(series, episode)], extension)
         except KeyError:
-            print 'Could not rename "%s"' % filename
+            print 'Episode name for  "%s" not found.' % filename
             continue
         new_filename = re.sub("[\\\/\:\*\"\?\<\>\|]", "", new_filename)
         print "Renaming \"%s\" to \"%s\"..." % (filename, new_filename.encode("ascii", "replace"))
@@ -83,9 +99,10 @@ def rename_files(episode_names, preview=False, use_ap=False):
                 os.rename(filename, new_filename)
 
 def main():
-    parser = optparse.OptionParser(usage="%prog [options] <IMDB id>", version="Episode renamer %s\nThis program is released under the GNU GPL." % VERSION)
+    parser = optparse.OptionParser(usage="%prog [options] <show id from the URL>", version="Episode renamer %s\nThis program is released under the GNU GPL." % VERSION)
     parser.add_option("-e", "--use-epguides", dest="use_epguides", action="store_true", help="use epguides.com instead of IMDB")
     parser.add_option("-p", "--preview", dest="preview", action="store_true", help="don't actually rename anything")
+    parser.add_option("-g", "--google", dest="google", action="store_true", help="search Google for the episode list based on the show name")
     parser.add_option("-a", "--use-atomic-parsley", dest="use_atomic_parsley", action="store_true", help="use AtomicParsley to fill in the files' tags")
     parser.set_defaults(preview=False)
     (options, arguments)=parser.parse_args()
@@ -99,7 +116,12 @@ def main():
     else:
         site = SITES[1]
 
-    page_url = site["url"] % arguments[0]
+    if options.google:
+        show_id = search_show(arguments[0], site)
+        print "Found show ID \"%s\"..." % show_id
+        page_url = site["url"] % show_id
+    else:
+        page_url = site["url"] % arguments[0]
 
     try:
         page = urllib2.urlopen(page_url).read()

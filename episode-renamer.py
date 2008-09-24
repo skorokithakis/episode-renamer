c0456f21b3e829f7a64241397f014289e92761c3
Stavros Korokithakis <stavros@korokithakis.net>
2008-09-24 04:06:29 +0300

    Improved parsers.

--- episode-renamer.py
+++ episode-renamer.py
@@ -16,18 +16,54 @@ import htmlentitydefs
 from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
 from version import *
 
+
+class Show:
+    def __init__(self, title="", rating=0):
+        self.title = title
+        self.episodes = {}
+
+def parse_imdb(page):
+    """Parse an IMDB page."""
+    # Remove &#160; (nbsp) entities.
+    page = page.replace("&#160;", "")
+    soup = BeautifulSoup(page, convertEntities=BeautifulStoneSoup.XML_ENTITIES)
+    title = re.search("\"?(.*?)\"? *?\(.*?\)", soup.title.string).group(1)
+    show = Show(title)
+
+    table = soup.find("h4").findNext("table")
+    for tr in table.findChildren("tr")[1:]:
+        tds = tr.findChildren("td")
+        season, episode = tds[0].contents[0].split(".")
+        show.episodes[(int(season), int(episode))] = {"title": tds[1].a.contents[0]}
+        show.episodes[(int(season), int(episode))]["rating"] = tds[2].contents[0]
+
+    return show
+
+def parse_epguides(page):
+    """Parse an epguides page."""
+    soup = BeautifulSoup(page, convertEntities=BeautifulStoneSoup.XML_ENTITIES)
+    page = unicode(soup).replace("\n", "")
+    show = Show()
+    try:
+        show.title = re.search("""<h1><a href="http://.*?">(.*?)</a></h1>""", page).groups()[0]
+    except AttributeError:
+        print "Could not find show title, cannot continue."
+        sys.exit()
+    episodes = re.findall("\d+. +(?P<season>\d+) *\- *(?P<episode>\d+).*?\d+ +[A-Za-z]+ +\d+ +<a target.*?>(?P<name>.*?)</a>", page)
+    for season, episode, name in episodes:
+        show.episodes[(int(season), int(episode))] = {"title": name}
+    return show
+
 SITES = [
     {"url": "http://epguides.com/%s/",
-     "episode_re": "\d+. +(?P<season>\d+) *\- *(?P<episode>\d+).*?\d+ +[A-Za-z]+ +\d+ +<a target.*?>(?P<name>.*?)</a>",
-     "title_re": """<h1><a href="http://.*?">(.*?)</a></h1>""",
      "domain": "epguides.com",
      "urlparser": "epguides.com\/(.*?)\/",
+     "parser": parse_epguides,
      },
     {"url": "http://www.imdb.com/title/%s/epdate",
-     "episode_re": """<td align="right" bgcolor="#eeeeee">(?P<season>\d+)\.(?P<episode>\d+).?</td> *<td><a .*?>(?P<name>.*?)</a></td>""",
-     "title_re": "<title>\"?(.*?)\"? *?\(.*?\)</title>",
      "domain": "imdb.com",
      "urlparser": "imdb\.com\/title\/(.*?)\/",
+     "parser": parse_imdb,
     },
         ]
 
@@ -42,31 +78,15 @@ def search_show(name, site):
 
     soup = BeautifulSoup(page)
     result = soup.find("a", "l")["href"]
-    print result
+
     show_id = re.search(site["urlparser"], result).group(1)
     return show_id
 
-def parse_page(page, site):
-    """Parse a page, entering the episode names in a dictionary."""
-    soup = BeautifulSoup(page, convertEntities=BeautifulStoneSoup.XML_ENTITIES)
-    episode_names = {}
-    page = unicode(soup).replace("\n", "")
-    try:
-        episode_names["title"] = re.search(site["title_re"], page).groups()[0]
-    except AttributeError:
-        print "Could not find show title, cannot continue."
-        sys.exit()
-    episodes = re.findall(site["episode_re"], page)
-    for season, episode, name in episodes:
-        episode_names[(int(season), int(episode))] = name
-    return episode_names
-
-def rename_files(episode_names, preview=False, use_ap=False):
+def rename_files(show, file_mask, preview=False, use_ap=False):
     series_parser = [
         re.compile("^.*?s *(?P<series>\d+) *e *(?P<episode>\d+).*\.(?P<extension>.*?)$", re.IGNORECASE),
         re.compile("^.*?(?P<series>\d+)x(?P<episode>\d+).*\.(?P<extension>.*?)$", re.IGNORECASE),
         ]
-    title = episode_names["title"]
     for filename in os.listdir("."):
         for parser in series_parser:
             matches = parser.search(filename)
@@ -82,8 +102,15 @@ def rename_files(episode_names, preview=False, use_ap=False):
         episode = int(match_dict["episode"])
         extension = match_dict["extension"]
 
+        info_dictionary = {"show": show.title,
+                           "series_num": series,
+                           "episode_num": episode,
+                           "extension": extension}
+
+        info_dictionary.update(show.episodes[(series, episode)])
+
         try:
-            new_filename = u"%s - S%02dE%02d - %s.%s" % (title, series, episode, episode_names[(series, episode)], extension)
+            new_filename = file_mask % info_dictionary
         except KeyError:
             print 'Episode name for  "%s" not found.' % filename
             continue
@@ -94,7 +121,7 @@ def rename_files(episode_names, preview=False, use_ap=False):
                 # The temp_filename shenanigans are necessary because AP sometimes
                 # chokes if it's set to overwrite the file.
                 temp_filename = filename + str(random.randint(10000, 99999))
-                proc = subprocess.Popen(("AtomicParsley", filename, "-o", temp_filename, "--TVShowName", episode_names["title"], "--stik", "TV Show", "--TVSeasonNum", str(series), "--TVEpisodeNum", str(episode), "--TVEpisode", episode_names[(series, episode)], "--title", episode_names[(series, episode)]))
+                proc = subprocess.Popen(("AtomicParsley", filename, "-o", temp_filename, "--TVShowName", show.title, "--stik", "TV Show", "--TVSeasonNum", str(series), "--TVEpisodeNum", str(episode), "--TVEpisode", show.episodes[(series, episode)]["title"], "--title", show.episodes[(series, episode)]["title"]))
                 proc.wait()
                 os.remove(filename)
                 try:
@@ -109,10 +136,34 @@ def rename_files(episode_names, preview=False, use_ap=False):
 
 def main():
     parser = optparse.OptionParser(usage="%prog [options] <show id from the URL>", version="Episode renamer %s\nThis program is released under the GNU GPL." % VERSION)
-    parser.add_option("-e", "--use-epguides", dest="use_epguides", action="store_true", help="use epguides.com instead of IMDB")
-    parser.add_option("-p", "--preview", dest="preview", action="store_true", help="don't actually rename anything")
-    parser.add_option("-g", "--google", dest="google", action="store_true", help="search Google for the episode list based on the show name")
-    parser.add_option("-a", "--use-atomic-parsley", dest="use_atomic_parsley", action="store_true", help="use AtomicParsley to fill in the files' tags")
+    parser.add_option("-e",
+                      "--use-epguides",
+                      dest="use_epguides",
+                      action="store_true",
+                      help="use epguides.com instead of IMDB")
+    parser.add_option("-p",
+                      "--preview",
+                      dest="preview",
+                      action="store_true",
+                      help="don't actually rename anything")
+    parser.add_option("-m",
+                      "--mask",
+                      dest="mask",
+                      default="%(show)s - S%(series_num)02dE%(episode_num)02d - %(title)s.%(extension)s",
+                      metavar="MASK",
+                      action="store",
+                      type="string",
+                      help="the filename mask to use when renaming (default: \"%default\")")
+    parser.add_option("-g",
+                      "--google",
+                      dest="google",
+                      action="store_true",
+                      help="search Google for the episode list based on the show name")
+    parser.add_option("-a",
+                      "--use-atomic-parsley",
+                      dest="use_atomic_parsley",
+                      action="store_true",
+                      help="use AtomicParsley to fill in the files' tags")
     parser.set_defaults(preview=False)
     (options, arguments)=parser.parse_args()
 
@@ -138,8 +189,8 @@ def main():
         print "An HTTP error occurred, HTTP code %s." % error.code
         sys.exit()
 
-    episode_names = parse_page(page, site)
-    rename_files(episode_names, options.preview, options.use_atomic_parsley)
+    show = site["parser"](page)
+    rename_files(show, options.mask, options.preview, options.use_atomic_parsley)
     print "Done."
 
 if __name__ == "__main__":
